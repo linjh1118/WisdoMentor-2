4800be01c5e44b1bba24fc5c0268758bbb3c15de
wesar <1982047268@qq.com>
2024-05-07 22:44:26 +0800

    修改

---
+++ wm_rag/prompt/llmlingua_zip.py
@@ -0,0 +1,14 @@
+import llmlingua
+from .base import Prompt
+from typing import List
+from langchain_core.documents import Document
+
+class llmlingua_zip(Prompt):
+    def __init__(self, model_path: str) -> None:
+        super().__init__()
+
+    def prompt_zip(self, prompt) -> str:
+        return super().prompt_zip(prompt)
+    
+    def prompt_gen(self, query, documents: List[Document]) -> str:
+        return super().prompt_gen(query, documents)
