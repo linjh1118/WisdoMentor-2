05f563b061d2f0370db2da83bb31a5a1dc70bf31
wesar <1982047268@qq.com>
2024-05-07 23:09:54 +0800

    111

--- wm_rag/prompt/llmlingua_zip.py
+++ wm_rag/prompt/llmlingua_zip.py
@@ -17,7 +17,3 @@ class llmlingua_zip(Prompt):
     
     def prompt_gen(self, query, documents: List[Document]) -> str:
         return super().prompt_gen(query, documents)
-    
-    def test_llmlingua(self):
-        test_llmlingua = "11111111111"
-        print(test_llmlingua)
