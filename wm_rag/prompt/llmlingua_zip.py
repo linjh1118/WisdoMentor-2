260bcbfc66649357ba1ca1afb6290880b2272c08
wesar <1982047268@qq.com>
2024-05-09 14:50:35 +0800

    111

--- wm_rag/prompt/llmlingua_zip.py
+++ wm_rag/prompt/llmlingua_zip.py
@@ -6,11 +6,11 @@ from langchain_core.documents import Document
 class llmlingua_zip(Prompt):
     def __init__(self, model_path: str) -> None:
         super().__init__()
-
-    def prompt_zip(self, prompt) -> str:
-        print(11111111111111111)
-        
-        return super().prompt_zip(prompt)
     
     def prompt_gen(self, query, documents: List[Document]) -> str:
         return super().prompt_gen(query, documents)
+    
+    def prompt_zip(self, query, documents: List[Document]) -> str:
+        print(11111111111111111)
+        
+        return super().prompt_zip(prompt)
