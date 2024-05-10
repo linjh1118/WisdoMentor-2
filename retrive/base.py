f63f431a2edd2189cd28733603e8cd2b5310ae48
Jimgannnnnn <156571239+Jimgannnnnn@users.noreply.github.com>
2024-05-10 22:09:50 +0800

    Add files via upload

---
+++ retrive/base.py
@@ -0,0 +1,12 @@
+from typing import List
+from langchain_core.documents import Document
+
+class Retrive:
+    def __init__(self) -> None:
+        return
+    
+    def recall(self, embed_query: List[float]) -> List[Document]:
+        return []
+    
+    def rerank(self, documents: List[Document]) -> List[Document]:
+        return documents
